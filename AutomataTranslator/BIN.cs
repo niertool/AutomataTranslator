62a13a87eed801526792d478deda458a2e6811fa
xxxxxx <xxxxxx@xxx.com>
2017-07-06 07:37:53 +0900

    Revert "Better String Detection"
    This reverts commit b00bf8661e0c2f832f0edd8bd89357ca455d849b.

--- AutomataTranslator/BIN.cs
+++ AutomataTranslator/BIN.cs
@@ -38,7 +38,7 @@ namespace AutomataTranslator {
             List<string> Strings = new List<string>();
             while (true) {
                 string String = ReadString(Reader.BaseStream);
-                if (String == string.Empty || String.Contains("_")) {
+                if (String == string.Empty) {
                     long Pos = Reader.BaseStream.Position;
                     bool Result = false;
                     try {
